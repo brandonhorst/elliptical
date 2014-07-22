c5c1a39a4c967517fc5f6a0adc4cfdf59a8b6324
= <brandonhorst@gmail.com>
2014-07-22 16:57:01 +0200

    Converted run tests to JS - all tests now in JS

---
+++ test/run.js
@@ -0,0 +1,52 @@
+var _ = require('lodash');
+var async = require('async');
+var moment = require('moment');
+var chai = require('chai');
+var expect = chai.expect;
+var lacona;
+var sinon = require('sinon');
+
+chai.use(require('sinon-chai'));
+
+if (typeof window !== 'undefined' && window.lacona) {
+	lacona = window.lacona;
+} else {
+	lacona = require('../src/lacona');
+}
+
+describe('run', function () {
+	var parser;
+
+	beforeEach(function() {
+		parser = new lacona.Parser();
+	});
+
+	it('can run an inputOption', function (done) {
+		var grammar = {
+			scope: {
+				run: sinon.spy(function (result, done) {
+					done();
+				})
+			},
+			schema: {
+				root: 'test',
+				run: 'run'
+			}
+		}
+
+		var afterRun = function (err) {
+			expect(err).to.not.exist;
+			expect(grammar.scope.run).to.have.been.called.once;
+			done();
+		}
+
+		var onData = sinon.spy(function (data) {
+			lacona.run(data, afterRun);
+		});
+
+		parser
+		.understand(grammar)
+		.on('data', onData)
+		.parse('test');
+	});
+});
