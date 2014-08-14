d10a23bb628f5392e6bb599c281949070424e7d9
= <brandonhorst@gmail.com>
2014-08-14 09:10:01 -0400

    Refactored gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
 var gulp = require('gulp');
 
-var browserify = require('gulp-browserify');
-var clean = require('gulp-clean');
+var async = require('async');
+var browserify = require('browserify');
+var exec = require('child_process').exec;
+var glob = require('glob');
+var merge = require('merge-stream');
+var package = require('./package');
+var path = require('path');
+var rimraf = require('rimraf');
+
+var filesize = require('gulp-filesize');
 var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
-var package = require('./package');
 var phantom = require('gulp-mocha-phantomjs');
 var rename = require('gulp-rename');
+var source = require('vinyl-source-stream');
+var streamify = require('gulp-streamify');
 var stylish = require('jshint-stylish');
 var watch = require('gulp-watch');
 var uglify = require('gulp-uglify');
@@ -24,10 +33,16 @@ gulp.task('phantom', ['build-browser-tests'], function() {
 		.pipe(phantom({reporter: 'dot'}));
 });
 
-gulp.task('clean', function() {
+gulp.task('clean', function(done) {
+	async.each(['dist', 'tmp'], function (dir, done) {
+		rimraf(dir, done);
+	}, done)
+});
+
+gulp.task('size', ['make'], function() {
 	return gulp
-		.src(['dist', 'tmp', 'coverage'], { read: false })
-		.pipe(clean());
+		.src('dist/**/*.js')
+		.pipe(filesize());
 });
 
 gulp.task('lint', function() {
@@ -39,24 +54,66 @@ gulp.task('lint', function() {
 });
 
 gulp.task('build-browser-tests', ['browserify'], function() {
-	return gulp
-		.src('test/**/*.js', {read: false})
-		.pipe(browserify({ignore: ['../src/lacona']}))
-		.pipe(rename({extname: '.browserify.js'}))
-		.pipe(gulp.dest('tmp'));
+	glob('./test/**/*.js', function(err, files) {
+		if (err) {
+			throw err;
+		} else {
+			var ify = browserify()
+			for (var i = 0; i < files.length; i++) {
+				ify.add(files[i]);
+			}
+			ify
+				.ignore('../lib/lacona')
+				.bundle()
+				.pipe(source('tests.js'))
+				.pipe(gulp.dest('tmp'));
+		}
+	});
 });
 
-gulp.task('browserify', function() {
-	return gulp
-		.src('lib/lacona.js', {read: false})
-		.pipe(browserify({standalone: 'lacona'}))
-		.pipe(rename('lacona.js'))
-		.pipe(gulp.dest('dist'));
-});
-gulp.task('make', ['browserify'], function() {
-	return gulp
-		.src('dist/lacona.js')
-		.pipe(uglify())
+gulp.task('make', function() {
+	return browserify({standalone: 'lacona'})
+		.add('./lib/lacona.js')
+		.bundle()
+		.pipe(source('lacona.js'))
+		.pipe(gulp.dest('dist'))
 		.pipe(rename('lacona.min.js'))
+		.pipe(streamify(uglify()))
 		.pipe(gulp.dest('dist'));
+
+	//This could should work, but I'm not going to get into filesize opt yet
+
+	// var noLodash = browserify({standalone: 'lacona'})
+	// 	.add('./lib/lacona.js')
+	// 	.ignore('lodash')
+	// 	.bundle()
+	// 	.pipe(source('lacona.external-lodash.js'))
+	// 	.pipe(gulp.dest('dist'))
+	// 	.pipe(rename('lacona.external-lodash.min.js'))
+	// 	.pipe(streamify(uglify()))
+	// 	.pipe(gulp.dest('dist'));
+
+	// var noAsync = browserify({standalone: 'lacona'})
+	// 	.add('./lib/lacona.js')
+	// 	.ignore('async')
+	// 	.bundle()
+	// 	.pipe(source('lacona.external-async.js'))
+	// 	.pipe(gulp.dest('dist'))
+	// 	.pipe(rename('lacona.external-async.min.js'))
+	// 	.pipe(streamify(uglify()))
+	// 	.pipe(gulp.dest('dist'));
+
+	// var noLodashNoAsync = browserify({standalone: 'lacona'})
+	// 	.add('./lib/lacona.js')
+	// 	.ignore('lodash')
+	// 	.ignore('async')
+	// 	.bundle()
+	// 	.pipe(source('lacona.external-lodash-async.js'))
+	// 	.pipe(gulp.dest('dist'))
+	// 	.pipe(rename('lacona.external-lodash-async.min.js'))
+	// 	.pipe(streamify(uglify()))
+	// 	.pipe(gulp.dest('dist'));
+
+	// return merge(lacona, noLodash, noAsync, noLodashNoAsync);
+
 });
