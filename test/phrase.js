09f42e62e80ce6c856e28ab9e890cd317d33ce3b
= <brandonhorst@gmail.com>
2014-08-31 22:45:39 -0400

    Converted test/phrase.js to spaces

--- test/phrase.js
+++ test/phrase.js
@@ -8,45 +8,45 @@ var sinon = require('sinon');
 chai.use(require('sinon-chai'));
 
 if (typeof window !== 'undefined' && window.lacona) {
-	lacona = window.lacona;
+  lacona = window.lacona;
 } else {
-	lacona = require('../lib/lacona');
+  lacona = require('../lib/lacona');
 }
 
 describe('Parser', function () {
-	var parser;
-	beforeEach(function() {
-		parser = new lacona.Parser({sentences: ['test']});
-	});
+  var parser;
+  beforeEach(function() {
+    parser = new lacona.Parser({sentences: ['test']});
+  });
 
-	it('handles phrases with extension', function (done) {
-		var grammar = {
-			phrases: [{
-				name: 'extended',
-				root: 'test'
-			}, {
-				name: 'extender',
-				extends: ['extended'],
-				root: 'totally'
-			}, {
-				name: 'test',
-				root: {type: 'extended'}
-			}]
-		}
+  it('handles phrases with extension', function (done) {
+    var grammar = {
+      phrases: [{
+        name: 'extended',
+        root: 'test'
+      }, {
+        name: 'extender',
+        extends: ['extended'],
+        root: 'totally'
+      }, {
+        name: 'test',
+        root: {type: 'extended'}
+      }]
+    }
 
-		var onData = sinon.spy(function (data) {
-			expect(['test', 'totally']).to.contain(data.suggestion.words[0].string);
-		});
+    var onData = sinon.spy(function (data) {
+      expect(['test', 'totally']).to.contain(data.suggestion.words[0].string);
+    });
 
-		var onEnd = function() {
-			expect(onData).to.have.been.calledTwice;
-			done();
-		}
+    var onEnd = function() {
+      expect(onData).to.have.been.calledTwice;
+      done();
+    }
 
-		parser
-		.understand(grammar)
-		.on('data', onData)
-		.on('end', onEnd)
-		.parse('t');
-	});
+    parser
+    .understand(grammar)
+    .on('data', onData)
+    .on('end', onEnd)
+    .parse('t');
+  });
 });
