02b1cb8f6ace1783165f663465b99c57c26f2276
Brandon Horst <brandonhorst@gmail.com>
2015-09-10 09:41:36 -0400

    Fix weird indentation on test/freetext

--- test/freetext.jsx
+++ test/freetext.jsx
@@ -10,104 +10,104 @@ import sinonChai from 'sinon-chai'
 chai.use(sinonChai)
 
 describe('freetext', () => {
-	var parser
+  var parser
 
-	beforeEach(() => {
-		parser = new Parser()
-	})
+  beforeEach(() => {
+    parser = new Parser()
+  })
 
-	it('validates input', () => {
-		function validate (input) {
-			return input === 'validValue'
-		}
+  it('validates input', () => {
+    function validate (input) {
+      return input === 'validValue'
+    }
 
-		parser.grammar = <freetext validate={validate} />
+    parser.grammar = <freetext validate={validate} />
 
-		const data1 = parser.parseArray('validValue')
-		expect(data1).to.have.length(1)
-		expect(text(data1[0])).to.equal('validValue')
-		expect(data1[0].result).to.equal('validValue')
+    const data1 = parser.parseArray('validValue')
+    expect(data1).to.have.length(1)
+    expect(text(data1[0])).to.equal('validValue')
+    expect(data1[0].result).to.equal('validValue')
 
-		const data2 = parser.parseArray('invalidValue')
-		expect(data2).to.have.length(0)
-	})
+    const data2 = parser.parseArray('invalidValue')
+    expect(data2).to.have.length(0)
+  })
 
-	it('no validate always accepts', () => {
-		parser.grammar = <freetext id='test' />
+  it('no validate always accepts', () => {
+    parser.grammar = <freetext id='test' />
 
-		const data = parser.parseArray('anything')
-		expect(data).to.have.length(1)
-		expect(text(data[0])).to.equal('anything')
-		expect(data[0].result).to.equal('anything')
-	})
+    const data = parser.parseArray('anything')
+    expect(data).to.have.length(1)
+    expect(text(data[0])).to.equal('anything')
+    expect(data[0].result).to.equal('anything')
+  })
 
-	it('allows consumeAll', () => {
-		const valSpy = spy()
+  it('allows consumeAll', () => {
+    const valSpy = spy()
 
-		function validate (input) {
-			valSpy()
-			return input === 'validValue'
-		}
+    function validate (input) {
+      valSpy()
+      return input === 'validValue'
+    }
 
-		parser.grammar = <freetext validate={validate} consumeAll={true} />
+    parser.grammar = <freetext validate={validate} consumeAll={true} />
 
-		const data = parser.parseArray('validValue')
-		expect(data).to.have.length(1)
-		expect(text(data[0])).to.equal('validValue')
-		expect(valSpy).to.have.been.calledOnce
-	})
+    const data = parser.parseArray('validValue')
+    expect(data).to.have.length(1)
+    expect(text(data[0])).to.equal('validValue')
+    expect(valSpy).to.have.been.calledOnce
+  })
 
-	it('allows splits on strings', () => {
-		class Test extends Phrase {
-			describe() {
-				return (
-					<sequence>
-						<freetext splitOn=' ' id='validator' />
-						<choice>
-							<literal text=' test' />
-							<literal text='thing' />
-						</choice>
-					</sequence>
-				)
-			}
-		}
+  it('allows splits on strings', () => {
+    class Test extends Phrase {
+      describe () {
+        return (
+          <sequence>
+            <freetext splitOn=' ' id='validator' />
+            <choice>
+              <literal text=' test' />
+              <literal text='thing' />
+            </choice>
+          </sequence>
+        )
+      }
+    }
 
-		parser.grammar = <Test />
+    parser.grammar = <Test />
 
-		const data = parser.parseArray('anything goes test')
-		expect(data).to.have.length(3)
-		expect(text(data[0])).to.equal('anything goes test')
-		expect(data[0].result.validator).to.equal('anything goes')
-		expect(text(data[1])).to.equal('anything goes test test')
-		expect(data[1].result.validator).to.equal('anything goes test')
-		expect(text(data[2])).to.equal('anything goes testthing')
-		expect(data[2].result.validator).to.equal('anything goes test')
-	})
+    const data = parser.parseArray('anything goes test')
+    expect(data).to.have.length(3)
+    expect(text(data[0])).to.equal('anything goes test')
+    expect(data[0].result.validator).to.equal('anything goes')
+    expect(text(data[1])).to.equal('anything goes test test')
+    expect(data[1].result.validator).to.equal('anything goes test')
+    expect(text(data[2])).to.equal('anything goes testthing')
+    expect(data[2].result.validator).to.equal('anything goes test')
+  })
 
-	it('allows splits on regex (with weird parens)', () => {
-		class Test extends Phrase {
-			describe() {
-				return (
-					<sequence>
-						<freetext splitOn={/(( )())/} id='validator' />
-						<choice>
-							<literal text=' test' />
-							<literal text='thing' />
-						</choice>
-					</sequence>
-				)
-			}
-		}
+  it('allows splits on regex (with weird parens)', () => {
+    class Test extends Phrase {
+      describe () {
+        return (
+          <sequence>
+            <freetext splitOn={/(( )())/} id='validator' />
+            <choice>
+              <literal text=' test' />
+              <literal text='thing' />
+            </choice>
+          </sequence>
+        )
+      }
+    }
 
-		parser.grammar = <Test />
+    parser.grammar = <Test />
 
-		const data = parser.parseArray('anything goes test')
-		expect(data).to.have.length(3)
-		expect(text(data[0])).to.equal('anything goes test')
-		expect(data[0].result.validator).to.equal('anything goes')
-		expect(text(data[1])).to.equal('anything goes test test')
-		expect(data[1].result.validator).to.equal('anything goes test')
-		expect(text(data[2])).to.equal('anything goes testthing')
-		expect(data[2].result.validator).to.equal('anything goes test')
-	})
+    const data = parser.parseArray('anything goes test')
+    expect(data).to.have.length(3)
+    expect(text(data[0])).to.equal('anything goes test')
+    expect(data[0].result.validator).to.equal('anything goes')
+    expect(text(data[1])).to.equal('anything goes test test')
+    expect(data[1].result.validator).to.equal('anything goes test')
+    expect(text(data[2])).to.equal('anything goes testthing')
+    expect(data[2].result.validator).to.equal('anything goes test')
+  })
 })
