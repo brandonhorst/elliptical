40f45acc4f0ff5b3d31598b8d5959ad5a13d126c
Brandon Horst <brandonhorst@gmail.com>
2016-05-25 17:58:48 -0400

    Improve error handling (include stack trace)

--- src/compile.js
+++ src/compile.js
@@ -14,9 +14,9 @@ function compileAndAddToMap (subElement, process, elementMap) {
   try {
     traverse = compileNonRoot(subElement, process)
   } catch (e) {
-    console.log(`Element failed compilation. Element: ${JSON.stringify(subElement, null, 1)}`)
-    console.error(e)
-    return subElement
+    const message = `Element failed compilation. Element: ${JSON.stringify(subElement)}, Message: ${e.message}`
+    e.message = message
+    throw e
   }
 
   elementMap.set(subElement, traverse)
